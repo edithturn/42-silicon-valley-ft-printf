ac023a2d84f8ef60b5dd9dcf2af89fbeafd07109
edithturn <edithpuclla20@gmail.com>
2020-07-16 12:11:02 -0700

    conversor to char

--- test_files/main.c
+++ test_files/main.c
@@ -6,7 +6,7 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 01:07:37 by epuclla           #+#    #+#             */
-/*   Updated: 2020/07/16 11:52:16 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/07/16 11:57:28 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int main()
 {
 	//ft_printf("Edith has %d cats\n", 42);
-	ft_printf("My name is %s Puclla\n", "Edith");
+	//ft_printf("My name is %s Puclla\n", "Edith");
+	ft_printf("My name start with %c letter\n", 'E');
 	return (0);
 }
