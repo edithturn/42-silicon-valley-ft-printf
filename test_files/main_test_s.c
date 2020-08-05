4baaed4e5bd784a84f39912bc9c72ca245145c9b
edithturn <edithpuclla20@gmail.com>
2020-08-04 18:33:02 -0700

    remove returns

--- test_files/main_test_s.c
+++ test_files/main_test_s.c
@@ -6,7 +6,7 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 01:07:37 by epuclla           #+#    #+#             */
-/*   Updated: 2020/08/03 11:43:16 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/08/04 07:45:08 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,6 @@
 
 int main()
 {
-	char c;
-	
-	c = 'A';
-	
-
-
 	//No Flag
 	/*ft_printf("%s|01|\n", "Puclla");
 	printf("%s|01|\n", "Puclla");*/
@@ -28,7 +22,7 @@ int main()
 	/*ft_printf("%-40s|01|\n", "Puclla");
 	printf("%-40s|01|\n", "Puclla");*/
 
-	ft_printf("%-9.3s|\n", "Edith");
-	printf("%-9.3s|\n", "Edith");
+	ft_printf("%-75.3s|\n", "Edith");
+	//printf("%-20.3s|\n", "Edith");
 	return (0);
 }
