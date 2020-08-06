92aab495c31cc6aea1d58e15cad7adbfb8305559
edithturn <edithpuclla20@gmail.com>
2020-08-06 13:09:21 -0700

    precision

--- test_files/main_test_s.c
+++ test_files/main_test_s.c
@@ -6,7 +6,7 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 01:07:37 by epuclla           #+#    #+#             */
-/*   Updated: 2020/08/06 12:35:53 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/08/06 12:58:34 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int main()
 	printf("%-75.10s|\n", "EdithEdithEdithEdith"); */
 
 	//ft_printf("%.5s|\n", "Edit");
-	printf("%.20s|\n", "Edit");
+	ft_printf("%-.20s|\n", "Edit");
 	//printf("%.2s|\n", "EdithEdithEdithEdith"); 
 
 	return (0);
