e0ba04889f69b420a7c255200f0789de424dff6e
edithturn <edithpuclla20@gmail.com>
2020-08-06 12:38:05 -0700

    add precision

--- test_files/main_test_s.c
+++ test_files/main_test_s.c
@@ -6,7 +6,7 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/11 01:07:37 by epuclla           #+#    #+#             */
-/*   Updated: 2020/08/06 10:51:15 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/08/06 12:35:53 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,9 @@ int main()
 	ft_printf("%-75.10s|\n", "EdithEdithEdithEdith");
 	printf("%-75.10s|\n", "EdithEdithEdithEdith"); */
 
-	ft_printf("%-*.10s|\n",75, "EdithEdithEdithEdith");
-	printf("%-*.10s|\n", 75, "EdithEdithEdithEdith"); 
+	//ft_printf("%.5s|\n", "Edit");
+	printf("%.20s|\n", "Edit");
+	//printf("%.2s|\n", "EdithEdithEdithEdith"); 
 
 	return (0);
 }
