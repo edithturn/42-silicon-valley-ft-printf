76338df081a2cceecb30f10cd5a2d2928658ee25
edithturn <edithpuclla20@gmail.com>
2020-08-23 17:27:18 -0700

    norminette for hex

--- source/ft_solve_hex.c
+++ source/ft_solve_hex.c
@@ -6,59 +6,60 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/04 19:10:21 by epuclla           #+#    #+#             */
-/*   Updated: 2020/08/23 07:25:08 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/08/23 17:25:17 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	h_itoa_hex(unsigned long long nbr, char format)
+void			h_itoa_hex(unsigned long long n, char format)
 {
-	if (nbr > 15)
+	if (n > 15)
 	{
-		h_itoa_hex(nbr / 16, format);
-		h_itoa_hex(nbr % 16, format);
+		h_itoa_hex(n / 16, format);
+		h_itoa_hex(n % 16, format);
 	}
 	else
 	{
-		if (nbr <= 9)
-			ft_putchar(nbr + '0');
+		if (n <= 9)
+			ft_putchar(n + '0');
 		else
-			format == 'x' ? ft_putchar(nbr - 10 + 'a') : ft_putchar(nbr - 10 + 'A');
+			format == 'x' ? ft_putchar(n - 10 + 'a') : ft_putchar(n - 10 + 'A');
 	}
 }
 
-static void		x_handle_flag(t_info *info, unsigned long long nbr, int nbrlen, int diff)
+static void		x_flag(t_info *info, unsigned long long n, int ln, int df)
 {
 	info->width++;
 	if (info->flag[e_minus] == '1')
 	{
-		ft_putnchar('0', diff);
-		if (info->point != 1 || nbr != 0)
-			h_itoa_hex(nbr, *info->format);
-		if (info->width > nbrlen)
-			while (--info->width > nbrlen + diff)
+		ft_putnchar('0', df);
+		if (info->point != 1 || n != 0)
+			h_itoa_hex(n, *info->format);
+		if (info->width > ln)
+			while (--info->width > ln + df)
 				ft_putchar(' ');
 	}
-	else {
-		if (info->width > nbrlen && (info->flag[e_zero] != '1' || info->point != 0))
-			while (--info->width > nbrlen + diff)
+	else
+	{
+		if (info->width > ln && (info->flag[e_zero] != '1' || info->point != 0))
+			while (--info->width > ln + df)
 				ft_putchar(' ');
-		if (info->width > nbrlen && info->flag[e_zero] == '1' && info->point == 0)
-			while (--info->width > nbrlen + diff)
+		if (info->width > ln && info->flag[e_zero] == '1' && info->point == 0)
+			while (--info->width > ln + df)
 				ft_putchar('0');
-		ft_putnchar('0', diff);
-		if (info->point != 1 || nbr != 0)
-			h_itoa_hex(nbr, *info->format);
+		ft_putnchar('0', df);
+		if (info->point != 1 || n != 0)
+			h_itoa_hex(n, *info->format);
 	}
 }
 
-void	ft_solve_hex(t_info *info)
+void			ft_solve_hex(t_info *info)
 {
 	unsigned long long	nbr;
 	unsigned long long	tmp;
-	int			nbrlen;
-	int diff;
+	int					nbrlen;
+	int					diff;
 
 	nbr = (unsigned int)(va_arg(info->arguments, unsigned long long));
 	tmp = nbr;
@@ -71,13 +72,13 @@ void	ft_solve_hex(t_info *info)
 		tmp = tmp / 16;
 	}
 	diff = info->precision - nbrlen;
-	if(diff < 0)
+	if (diff < 0)
 		diff = 0;
 	if (info->width <= info->precision)
 		info->total_length = info->total_length + diff;
 	if (info->width > nbrlen && info->width > info->precision)
-		info->total_length = info->total_length + (info->width - nbrlen);	
-	x_handle_flag(info, nbr, nbrlen, diff);
+		info->total_length = info->total_length + (info->width - nbrlen);
+	x_flag(info, nbr, nbrlen, diff);
 	info->total_length = info->total_length + nbrlen;
 	info->format++;
 }
