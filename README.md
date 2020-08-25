4189168a2f3868f929fbf58869b1d0a9b03a5cfc
edithturn <edithpuclla20@gmail.com>
2020-08-25 11:37:07 -0700

    add general flow and clean README

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
-# ft_printf
+# :palm_tree: :ear_of_rice: ft_printf
 
 ### Info:
 Recode printf function
 
 ### Printf Workflow
 
-See the workflow [HERE](https://drive.google.com/file/d/1zPrMQ2q4wuR16TWdBtKlLVVquNKdpGmb/view?usp=sharing)
+#### :bell: General WorkFlow
+![General Flow](img/graded_by_moulinete.png)
 
-### Compilation
+
+See workflow about each format specifier  [HERE](https://drive.google.com/file/d/1zPrMQ2q4wuR16TWdBtKlLVVquNKdpGmb/view?usp=sharing)
+
+## Compilation
 ```bash
 make all
-gcc  ./tests/main_test_c.c libftprintf.a  -I ./source/
-gcc  ./tests/main_test_di.c libftprintf.a  -I ./source/
+gcc  ./tests/01_main_test_s.c libftprintf.a  -I ./include/
 ```
 
-### Debug  Mode
+## Debug  Mode 
 
 - First: Change your Makefile, adding -g3
 ```c
@@ -35,36 +38,16 @@ lldb a.out
 b main
 run
 gui
+
 ```
+## Testers
 
-
-
-### Note
-- Guaradar Contexto
-- Mandar a Dispatcher
-	- Depende de si es d,s, c
-- Mandar a procesar toda la estructura del contexto
--https://stackoverflow.com/questions/48066271/how-to-print-a-pointer-address-without-
-- Readme : lo convierte en pagina web.
-- Datatype Union |https://www.tutorialspoint.com/cprogramming/c_unions.htm | Docosaurius | https://github.com/facebook/docusaurus
-- https://github.com/lgutter/ft_printf
-
-
-# Testers
-
-Recommended tester:
-- git clone https://github.com/gavinfielder/pft.git pft && echo "pft/" >> .gitignore
-
-Others
 - https://github.com/Mazoise/42TESTERS-PRINTF (passed)
 - https://github.com/gavinfielder/pft	(passed)
 - https://github.com/Kwevan/PRINTF_TESTER
-
-
 - https://github.com/charMstr/printf_lover_v2
 
-#Resources:
-
+## :fireworks: Resources:
 - https://docs.google.com/document/d/1znc7N5ua3WC1PhGcirRhtadHYSrPK3wxTApMdlJXYes/edit?pli=1#
 - https://www.cplusplus.com/reference/cstdio/printf/
 - https://www.cypress.com/file/54441/download
@@ -92,5 +75,6 @@ gcc ./tests/05_main_test_x.c libftprintf.a  -I ./include/
 gcc ./tests/06_main_test_u.c libftprintf.a  -I ./include/
 gcc ./tests/07_main_test_prc.c libftprintf.a -I ./include/
 
-valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes ./a.out
+valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-origins=yes --track-fds=yes ./a.out
+
 ```
--- tests/03_main_test_di.c
+++ tests/03_main_test_di.c
@@ -6,7 +6,7 @@
 /*   By: epuclla <epuclla@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/10 08:38:06 by epuclla           #+#    #+#             */
-/*   Updated: 2020/08/25 00:27:40 by epuclla          ###   ########.fr       */
+/*   Updated: 2020/08/25 10:04:26 by epuclla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,74 +21,76 @@ int main()
 
 	ft_printf("%s\n" , "|01| Just decimal");
 	ft_printf("|%d|\n", 52);
-   	//printf(" LENGTH printf    : %d\n\n", printf("|%d|", 52));
+   	printf(" LENGTH printf    : %d\n\n", printf("|%d|", 52));
 
 	ft_printf("%s\n" , "|02| Negative number");
 	ft_printf("|%d|\n", -2);
-   	//printf(" LENGTH printf    : %d\n\n", printf("|%d|", -2));
+   	printf(" LENGTH printf    : %d\n\n", printf("|%d|", -2));
 
 	ft_printf("%s\n" , "|03| Negative flag");
 	ft_printf("|%-d|\n", 3);
-   	//printf(" LENGTH printf    : %d\n\n", printf("|%-d|", 3));
+   	printf(" LENGTH printf    : %d\n\n", printf("|%-d|", 3));
 
 	ft_printf("%s\n" , "|04| Negative flag with width greater than number");
 	ft_printf("|%-4d|\n", 3);
-   	//printf(" LENGTH printf    : %d\n\n", printf("|%-4d|", 3));
+   	printf(" LENGTH printf    : %d\n\n", printf("|%-4d|", 3));
 
 	ft_printf("%s\n" , "|05| Negative flag with width less than number");
 	ft_printf("|%-2d|\n", 3000);
-   	//printf(" LENGTH printf    : %d\n\n", printf("|%-2d|", 3000));
+   	printf(" LENGTH printf    : %d\n\n", printf("|%-2d|", 3000));
 
 	ft_printf("%s\n" , "|06| Just width greater than nbrlen");
 	ft_printf(" LENGTH ft_printf : %d\n", ft_printf("|%8d|", 3));
-   	//printf(" LENGTH printf    : %d\n\n", printf("|%8d|", 3));
+   	printf(" LENGTH printf    : %d\n\n", printf("|%8d|", 3));
 
 	ft_printf("%s\n" , "~07~ Precision less than nbrlen");
 	ft_printf(" LENGTH ft_printf : %d\n", ft_printf("|%.1d|", 70));
-   	//printf(" LENGTH printf    : %d\n\n", printf("|%.1d|", 70));
+   	printf(" LENGTH printf    : %d\n\n", printf("|%.1d|", 70));
 
 	ft_printf("%s\n" , "~08~ Precision greater than nbrlen");
 	ft_printf(" LENGTH ft_printf : %d\n", ft_printf("|%.4d|", 7));
-   	//printf(" LENGTH printf    : %d\n\n", printf("|%.4d|", 7));
+   	printf(" LENGTH printf    : %d\n\n", printf("|%.4d|", 7));
 	
 	ft_printf("%s\n" , "~09~ Precision greater than nbrlen and flag [-]");
 	ft_printf(" LENGTH ft_printf : %d\n", ft_printf("|%-.4d|", 7));
-   	//printf(" LENGTH printf    : %d\n\n", printf("|%-.4d|", 7));
+   	printf(" LENGTH printf    : %d\n\n", printf("|%-.4d|", 7));
 
 	ft_printf("%s\n" , "~10~ Precision greater than nbrlen and flag [0]");
 	ft_printf(" LENGTH ft_printf : %d\n", ft_printf("|%0.4d|", 7));
-   	//printf(" LENGTH printf    : %d\n\n", printf("|%0.4d|", 7));
+   	printf(" LENGTH printf    : %d\n\n", printf("|%0.4d|", 7));
 
 	ft_printf("%s\n" , "~11~ Precision greater than nbrlen and negative number");
 	ft_printf(" LENGTH ft_printf : %d\n", ft_printf("|%.1d|", -30));
-   //printf(" LENGTH printf    : %d\n\n", printf("|%.1d|", -30));
+   printf(" LENGTH printf    : %d\n\n", printf("|%.1d|", -30));
 
 	ft_printf("%s\n" , "~12~ Flag [0]");
 	ft_printf(" LENGTH ft_printf : %d\n", ft_printf("|%-08.1d|", 3));
-   	//printf(" LENGTH printf    : %d\n\n", printf("|%-08.1d|", 3));
+   	printf(" LENGTH printf    : %d\n\n", printf("|%-08.1d|", 3));
 
 	ft_printf("%s\n" , "~13~ Flag [-], width and precision > nbrlen, precision > width");
 	ft_printf(" LENGTH printf    : %d\n\n", ft_printf("|%-5.8d|", 3));
-	//printf(" LENGTH printf    : %d\n\n", printf("|%-5.8d|", 3));
+	printf(" LENGTH printf    : %d\n\n", printf("|%-5.8d|", 3));
 
 	ft_printf("%s\n" , "~14~ Flag [-], width and precision > nbrlen, precision < width");
 	ft_printf(" LENGTH ft_printf : %d\n", ft_printf("|%-8.5d|", 3));
-	//printf(" LENGTH ft_printf : %d\n", printf("|%-8.5d|", 3));
+	printf(" LENGTH ft_printf : %d\n", printf("|%-8.5d|", 3));
 
 	ft_printf(" LENGTH ft_printf : %d\n", ft_printf("|%.3d|", 13862));
-   	//printf(" LENGTH printf    : %d\n\n", printf("|%.3d|", 13862));
+   	printf(" LENGTH printf    : %d\n\n", printf("|%.3d|", 13862));
 
 	//Fix Bug
 	ft_printf("|%-7d|\n", -14);
-	//printf("|%-7d|\n", -14);
+	printf("|%-7d|\n", -14);
 
 	//Elian
-	//ft_printf("|%4d|\n", 33);
-	//printf("|%4d|\n", 33);
-
+	ft_printf("|%4d|\n", 33);
+	printf("|%4d|\n", 33);
 	ft_printf("|%d|\n", 2147483648);
 	//printf("|%d|\n", 2147483648);
-
+	
+	// Specific test
+	ft_printf("|%0*.*d|\n", 3, -2, 8);
+    printf("|%0*.*d|\n", 3, -2, 8);
 
 	return (0);
 }
